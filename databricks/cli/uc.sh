7b1896f69de79c630a6ee344503368b217a11a96
David <david-khala@hotmail.com>
2024-11-03 13:27:31 +0000

    to test

--- databricks/cli/uc.sh
+++ databricks/cli/uc.sh
@@ -12,10 +12,19 @@ allowlist() {
     databricks artifact-allowlists get INIT_SCRIPT
 
     echo ">>> JAR"
-    databricks artifact-allowlists get LIBRARY_JAR 
-    
+    databricks artifact-allowlists get LIBRARY_JAR
+
     echo ">>> maven"
     databricks artifact-allowlists get LIBRARY_MAVEN
-
 }
+allow-script() {
+    databricks artifact-allowlists update INIT_SCRIPT --json "{\"artifact_matchers\": [{\"artifact\": \"$@\",\"match_type\": \"PREFIX_MATCH\"}]}"
+}
+allow-jar() {
+    databricks artifact-allowlists update LIBRARY_JAR --json "{\"artifact_matchers\": [{\"artifact\": \"$@\",\"match_type\": \"PREFIX_MATCH\"}]}"
+}
+allow-maven() {
+    databricks artifact-allowlists update LIBRARY_MAVEN --json "{\"artifact_matchers\": [{\"artifact\": \"$@\",\"match_type\": \"PREFIX_MATCH\"}]}"
+}
+
 $@
