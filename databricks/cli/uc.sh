d072e3e6115a9d63c0fbdba7f82d1a962e709344
David <david-khala@hotmail.com>
2024-11-03 07:16:48 +0000

    switch test enviornment

--- databricks/cli/uc.sh
+++ databricks/cli/uc.sh
@@ -6,5 +6,16 @@ schemas() {
     local catalog=${1:-$catalog}
     databricks schemas list $catalog --include-browse
 }
+allowlist() {
+    # artifact allowlist
+    echo ">>> init script"
+    databricks artifact-allowlists get INIT_SCRIPT
 
+    echo ">>> JAR"
+    databricks artifact-allowlists get LIBRARY_JAR 
+    
+    echo ">>> maven"
+    databricks artifact-allowlists get LIBRARY_MAVEN
+
+}
 $@
