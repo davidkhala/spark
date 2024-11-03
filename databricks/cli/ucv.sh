3014704109fae7fdbbe58775701167442920335e
David <david-khala@hotmail.com>
2024-11-03 05:19:49 +0000

    formatting

--- databricks/cli/ucv.sh
+++ databricks/cli/ucv.sh
@@ -5,10 +5,10 @@ catalog=${catalog:-$(sed 's/-/_/' <<<$workspace_name)}
 create-managed() {
     local volume=$1
     # create if not exists
-    if ! databricks volumes read $catalog.$schema.$volume > /dev/null; then
+    if ! databricks volumes read $catalog.$schema.$volume >/dev/null; then
         databricks volumes create $catalog $schema $volume MANAGED
     fi
-    databricks grants update volume $catalog.$schema.$volume --json '{"changes": [{"principal": "account users", "add": ["READ_VOLUME"]}]}'
+    databricks grants update volume $catalog.$schema.$volume --json '{"changes": [{"principal": "account users", "add": ["READ_VOLUME"]}]}' >/dev/null
 
 }
 
