a42c76eddf0c54524062381ed2e17a2739983c90
David <david-khala@hotmail.com>
2024-11-04 08:47:45 +0000

    fix params

--- databricks/cli/user.sh
+++ databricks/cli/user.sh
@@ -14,6 +14,7 @@ whoami() {
 }
 create-service-principal() {
     local name=$1
+    shift 1
     databricks service-principals create --active --display-name $name $@ | jq '{applicationId,id}' 
 
 }
