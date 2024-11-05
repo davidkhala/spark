6caf5d9d2a983131c1414933532c8cc19fb1e349
David <david-khala@hotmail.com>
2024-11-05 04:37:32 +0000

    rollback

--- databricks/cli/user.sh
+++ databricks/cli/user.sh
@@ -13,7 +13,9 @@ whoami() {
     databricks current-user me
 }
 create-service-principal() {
-    databricks service-principals create $@ | jq '{applicationId,id}'
+    local name=$1
+    shift 1
+    databricks service-principals create --active --display-name $name $@ | jq '{applicationId,id}'
 }
 list-service-principal() {
     databricks service-principals list -o json
