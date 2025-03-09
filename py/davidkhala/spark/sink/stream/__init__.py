eb42984d2219131c9d7adf13168a24d92400e93c
David Liu <david-khala@hotmail.com>
2025-03-10 03:01:12 +0800

    [TODO] run spark locally Add ForeachWriter

--- py/davidkhala/spark/sink/stream/__init__.py
+++ py/davidkhala/spark/sink/stream/__init__.py
@@ -1,11 +1,24 @@
+from abc import abstractmethod, ABC
+
 from pyspark.sql import DataFrame
+from pyspark.sql._typing import SupportsProcess, SupportsOpen, SupportsClose
+from pyspark.sql.types import Row
 from pyspark.sql.streaming import StreamingQuery
+
+
+class ForeachWriter(SupportsProcess, SupportsOpen, SupportsClose, ABC):
+    """
+    object class used in DataStreamWriter.foreach(...)
+    """
+
+    @abstractmethod
+    def process(self, row: Row) -> None: ...
 
 
 def show(df: DataFrame):
     assert df.isStreaming
 
-    def on_batch(_df:DataFrame, batch_id):
+    def on_batch(_df: DataFrame, batch_id):
         # TODO WIP
 
         import logging
