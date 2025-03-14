61890010c17a697cdd2a4821076f5abfc42b1c1b
David Liu <david-khala@hotmail.com>
2025-03-14 12:16:44 +0800

    backup

--- py/davidkhala/spark/sink/stream/__init__.py
+++ py/davidkhala/spark/sink/stream/__init__.py
@@ -1,37 +1,25 @@
 from abc import abstractmethod, ABC
+from typing import Callable
 
 from pyspark.sql import DataFrame
-from pyspark.sql._typing import SupportsProcess, SupportsOpen, SupportsClose
-from pyspark.sql.types import Row
 from pyspark.sql.streaming import StreamingQuery
+from pyspark.sql.types import Row
 
 
-class ForeachWriter(SupportsProcess, SupportsOpen, SupportsClose, ABC):
+class ForeachWriter(ABC):
+
     """
     object class used in DataStreamWriter.foreach(...)
     """
 
+    def open(self, partition_id: int, epoch_id: int) -> bool: ...
     @abstractmethod
     def process(self, row: Row) -> None: ...
 
+    def close(self, error: Exception) -> None: ...
 
-def show(df: DataFrame):
+def show(df: DataFrame, on_batch: Callable[[DataFrame, int], None]):
     assert df.isStreaming
 
-    def on_batch(_df: DataFrame, batch_id):
-        # TODO WIP
-
-        import logging
-        logging.basicConfig(level=logging.DEBUG,  # 控制台打印的日志级别
-                            filename='new.log',
-                            filemode='a')
-        logger = logging.getLogger(__name__)
-        logger.info(batch_id, _df.count())
-        # _df.writeTo('rate_stream').append() # works
-
-    query: StreamingQuery = (
-        df.writeStream
-        .foreachBatch(on_batch)
-        .start()
-    )
+    query: StreamingQuery = df.writeStream.foreachBatch(on_batch).start()
     return query
--- py/tests/test_connect.py
+++ py/tests/test_connect.py
@@ -1,13 +1,14 @@
 import os
 import unittest
 
+from databricks.sdk import WorkspaceClient
+from davidkhala.databricks.workspace.server import Cluster
 from pyspark.sql.connect.session import SparkSession
 
 from davidkhala.spark.connect import Databricks
+from davidkhala.spark.session import ServerMore
 from davidkhala.spark.sink.stream import show
 from davidkhala.spark.source.stream import sample
-
-
 
 
 class LocalTestCase(unittest.TestCase):
@@ -18,35 +19,45 @@ class LocalTestCase(unittest.TestCase):
 
     def test_local_remote(self):
         self.skipTest('WIp')
-        session = (SparkSession.builder
-                   .remote("sc://localhost:7077")
-                   .getOrCreate())
+        session = (SparkSession.builder.remote("sc://localhost:7077").getOrCreate())
         session.stop()
 
 
 def sample_stream_display(spark, density):
     df = sample(spark, density)
-    query = show(df)
+
+    def on_batch(df, id: int):
+        from davidkhala.newrelic.log import Ingestion
+        i = Ingestion(...)
+        i.send(f"type={type(df)}, count={df.count()}, batch_id={id}")
+
+    query = show(df, on_batch)
     query.awaitTermination()
 
 
 class DatabricksTestCase(unittest.TestCase):
-    def setUp(self):
-        cluster_id = "1216-151225-fxmvxmxo"
-        token = os.environ['PAT']
-        workspace_instance_name = os.environ['WORKSPACE']
-        self.spark = Databricks(workspace_instance_name, token, cluster_id)
+    spark: ServerMore
+
+    @classmethod
+    def setUpClass(cls):
+        cluster_id = os.environ.get('CLUSTER_ID') or "0314-031459-bpfyf6sg"
+        token = os.environ.get('PAT')
+        workspace_instance_name = os.environ.get('WORKSPACE') or "adb-4397269257097472.12.azuredatabricks.net"
+        cls.spark = Databricks(workspace_instance_name, token, cluster_id)
+
+        Cluster(WorkspaceClient(host=workspace_instance_name, token=token, cluster_id=cluster_id)).start()
 
     def test_cluster(self):
         df = self.spark.sql('select 1')
-        df.show()
         self.assertEqual('Databricks Shell', self.spark.appName)
+        df.show()
 
     def test_sample(self):
         sample_stream_display(self.spark, 1)
 
-    def tearDown(self):
-        self.spark.stop()
+    @classmethod
+    def tearDownClass(cls):
+        cls.spark.disconnect()
 
 
 if __name__ == '__main__':
