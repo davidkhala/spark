03c5395125f3f9c6c4b0970ca2cb9874247e94ac
David Liu <david-khala@hotmail.com>
2025-03-28 05:21:55 +0800

    Update and rename install.run to setup.sh

---
+++ vendor/ilum/setup.sh
@@ -0,0 +1,14 @@
+set -e
+install(){
+  helm repo add ilum https://charts.ilum.cloud
+  # you need a k8s cluster 
+  helm install ilum ilum/ilum
+}
+expose(){
+    local namespace=${1:-default}
+    kubectl port-forward svc/ilum-ui 9777:9777 -n $namespace
+}
+open(){
+  xdg-open http://localhost:31777
+}
+"$@"
