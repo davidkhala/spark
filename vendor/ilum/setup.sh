3e57ffd52bfa6c70c794be95a3b054f2f63965c6
David Liu <david-khala@hotmail.com>
2025-03-30 20:05:21 +0800

    backup

--- vendor/ilum/setup.sh
+++ vendor/ilum/setup.sh
@@ -1,20 +1,33 @@
 set -e
+namespace=${1:-default}
 install(){
   helm repo add ilum https://charts.ilum.cloud
   # you need a k8s cluster 
   helm install ilum ilum/ilum
 }
+setup-sql(){
+  # Enable SQL
+  helm upgrade ilum ilum/ilum --set ilum-sql.enabled=true --set ilum-core.sql.enabled=true
+}
+setup-metastore(){
+  # Enable Table Explorer (Hive Metastore)
+  # TODO To be fixed by official
+  helm upgrade ilum ilum/ilum --set ilum-hive-metastore.enabled=true --set ilum-core.hiveMetastore.enabled=true
+}
+setup-lineage(){
+  helm upgrade ilum ilum/ilum --set global.lineage.enabled=true --set ilum-marquez.web.enabled=true
+  kubectl port-forward svc/ilum-marquez-web 9444:9444
+}
+
 expose(){
-    local namespace=${1:-default}
     kubectl port-forward svc/ilum-ui 9777:9777 -n $namespace
+    kubectl port-forward svc/ilum-jupyter 8888:8888 -n $namespace
 }
 open(){
-  
   xdg-open http://localhost:31777
 }
-with-SQL(){
-  helm upgrade ilum ilum/ilum --set ilum-sql.enabled=true
-  
+uninstall(){
+  helm uninstall ilum
 }
 
 "$@"
