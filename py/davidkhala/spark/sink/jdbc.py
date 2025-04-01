d4d8fd19436b2a91fc8ffc6847c011ef63d0d960
David Liu <david-khala@hotmail.com>
2025-04-01 10:23:46 +0800

    davidkhala.spark: 0.1.1 fix default mode

--- py/davidkhala/spark/sink/jdbc.py
+++ py/davidkhala/spark/sink/jdbc.py
@@ -1,23 +1,28 @@
 from enum import Enum
+from typing import Union
 
 from pyspark.sql.connect.dataframe import DataFrame
 
 from davidkhala.spark.sink import Write
 
 
+class Mode(Enum):
+    append = "append"
+    overwrite = "overwrite"
+    ignore = "ignore"
+    error = "errorifexists"  # default. Throw an exception if data already exists.
+
+
 class SQLServer(Write):
-    class Mode(Enum):
-        append = "append"
-        overwrite = "overwrite"
 
     def __init__(self, df: DataFrame, *,
                  server: str, database: str, table: str, user: str, password: str,
-                 mode: Mode = Mode.append
+                 mode: Union[Mode,str] = Mode.error
                  ):
         super().__init__(df)
         self.url = f"jdbc:sqlserver://{server};databaseName={database}"
         self.table = table
-        self.mode = mode.value
+        self.mode = mode.value if isinstance(mode,Mode) else Mode(mode).value
         self.properties = {
             "user": user,
             "password": password,
