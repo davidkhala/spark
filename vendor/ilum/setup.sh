681bfc9c9e678a2a8d9796f022b09371e6a5dc53
David Liu <david-khala@hotmail.com>
2025-04-03 04:27:44 +0800

    Bugs found

--- vendor/ilum/setup.sh
+++ vendor/ilum/setup.sh
@@ -1,33 +1,42 @@
 set -e
-namespace=${1:-default}
-install(){
+namespace=${1:-ilum}
+register() {
+  # Register the repository
   helm repo add ilum https://charts.ilum.cloud
-  # you need a k8s cluster 
-  helm install ilum ilum/ilum
+  helm repo update
 }
-setup-sql(){
+install() {
+  # you need a k8s cluster
+  helm install ilum ilum/ilum -n $namespace
+}
+setup-sql() {
   # Enable SQL
-  helm upgrade ilum ilum/ilum --set ilum-sql.enabled=true --set ilum-core.sql.enabled=true
+  update --set ilum-sql.enabled=true --set ilum-core.sql.enabled=true
 }
-setup-metastore(){
+setup-metastore() {
   # Enable Table Explorer (Hive Metastore)
   # TODO To be fixed by official
-  helm upgrade ilum ilum/ilum --set ilum-hive-metastore.enabled=true --set ilum-core.hiveMetastore.enabled=true
+  update --set ilum-hive-metastore.enabled=true --set ilum-core.hiveMetastore.enabled=true --set ilum-sql.enabled=true --set ilum-core.sql.enabled=true
 }
-setup-lineage(){
-  helm upgrade ilum ilum/ilum --set global.lineage.enabled=true --set ilum-marquez.web.enabled=true
+setup-lineage() {
+  update --set global.lineage.enabled=true --set ilum-marquez.web.enabled=true
   kubectl port-forward svc/ilum-marquez-web 9444:9444
 }
-
-expose(){
-    kubectl port-forward svc/ilum-ui 9777:9777 -n $namespace
-    kubectl port-forward svc/ilum-jupyter 8888:8888 -n $namespace
+update() {
+  helm upgrade ilum ilum/ilum -n $namespace $@
 }
-open(){
+expose() {
+  kubectl port-forward svc/ilum-ui 9777:9777 -n $namespace
+  kubectl port-forward svc/ilum-jupyter 8888:8888 -n $namespace
+}
+open() {
   xdg-open http://localhost:31777
 }
-uninstall(){
-  helm uninstall ilum
+uninstall() {
+  helm uninstall ilum -n $namespace
+  # TODO leakage To be fixed by official
+  kubectl delete service ilum-hive-metastore
+  kubectl delete pvc --all -n $namespace
 }
 
 "$@"
